db2fd04188ceca7b6a4e9c04547b50f35267db00
yogurt14r <smurfinho03@gmail.com>
2025-02-24 22:48:12 +0900

    add Header and Footer

--- project2/tsi-course/src/App.jsx
+++ project2/tsi-course/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/blog" element={<Blog />}/>
+        </Routes>
+        
+        <Footer />
+      </BrowserRouter>
+      
     </>
   )
 }
---
+++ project2/tsi-course/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return <div className="footer">
+        © 2025 TSI
+    </div>
+}
+export default Footer
