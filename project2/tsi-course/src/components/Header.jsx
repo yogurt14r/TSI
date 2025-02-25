d76a5d22de15ee5dcbc1d5e6012a32421db83150
yogurt14r <smurfinho03@gmail.com>
2025-02-25 17:06:25 +0900

    q

--- project2/tsi-course/src/components/Header.jsx
+++ project2/tsi-course/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-    return <div className="header">
+    return <div className="flex gap-[50px] fixed top-0 left-0 w-full bg-[#333] text-white p-[25px] text-center text-[14pt] ">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
