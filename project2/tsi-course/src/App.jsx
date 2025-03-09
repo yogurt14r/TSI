36238c77ed8a9f34072223d8e0c4425600c4700e
yogurt14r <smurfinho03@gmail.com>
2025-03-09 15:57:30 +0900

    del

--- project2/tsi-course/src/App.jsx
+++ project2/tsi-course/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -26,7 +24,6 @@ function App() {
         
         <Footer />
       </BrowserRouter>
-      
     </>
   )
 }
--- project2/tsi-course/src/components/Footer.jsx
+++ project2/tsi-course/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return <div className="fixed bottom-0 left-0 w-full h-[250px] bg-[#333] text-white text-center p-[125px] text-[14px]">
+    return <div className="fixed bottom-0 left-0 w-full h-[250px] bg-[#2A303E] text-white text-center p-[125px] text-[14px]">
         © 2025 TSI
     </div>
 }
