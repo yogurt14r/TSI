b977c88b4659c1f7d88055a13a96456e16dd0750
yogurt14r <smurfinho03@gmail.com>
2025-03-13 23:03:38 +0900

    Update LoginModal.jsx

--- project2/tsi-course/src/components/LoginModal.jsx
+++ project2/tsi-course/src/components/LoginModal.jsx
@@ -4,21 +4,22 @@ const LoginModal = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/[0.5] bg-opacity-50 z-50">
-            <div className="w-[480px] bg-white text-black p-6 rounded-lg shadow-lg">
-                <h2 className="text-2xl font-bold mb-4">Вход</h2>
+        <div className="fixed inset-0 flex items-center justify-center bg-black/[0.5] z-50">
+            <div className="w-[480px] h-[600px] bg-white text-black p-6 rounded-[20px] shadow-lg relative flex flex-col justify-center">
+                <button onClick={onClose} className="absolute right-[-55px] top-0 h-[45px] w-[45px] bg-gray-300 text-black text-[18pt] rounded-[20px] hover:bg-gray-400">
+                    ×
+                </button>
+                <h2 className="text-3xl font-bold mb-4">Добро пожаловать!</h2>
+                <p className="text-[14pt] p-7">Введите номер телефона, чтобы войти или зарегистрироваться</p>
                 <form>
                     <div className="mb-4">
-                        <label className="block text-sm font-medium mb-2">Номер телефона</label>
-                        <input type="tel" className="w-full p-2 border rounded" placeholder="+7 (___) ___-__-__" />
+                        <label className="block text-sm font-medium mb-2 text-left">Номер телефона</label>
+                        <input type="tel" className="w-full h-[60px] p-2 border rounded-[20px]" placeholder="+7 (___) ___-__-__" />
                     </div>
-                    <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+                    <button type="submit" className="w-full h-[50px] bg-[#00e0c7] text-white p-2 rounded-[20px] hover:bg-[#006270] transition duration-300">
                         Получить код в СМС
                     </button>
                 </form>
-                <button onClick={onClose} className="mt-4 w-full bg-gray-300 text-black p-2 rounded hover:bg-gray-400">
-                    Закрыть
-                </button>
             </div>
         </div>
     );
