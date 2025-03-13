c36176e6f47da41d1ae0804f1afd5a4d0cd26fc6
yogurt14r <smurfinho03@gmail.com>
2025-03-13 23:04:07 +0900

    test Cards in Home

--- project2/tsi-course/src/pages/Home.tsx
+++ project2/tsi-course/src/pages/Home.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Card from '../components/Card';
 
 const Home = () => {
-    return (<>
+    return (
+    <>
         <div className='text-black'>Home</div>
+        <section className="flex flex-wrap max-w-[1200px] mx-auto">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </section>
     </>
     );
 }
