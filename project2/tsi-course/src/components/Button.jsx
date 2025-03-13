3860b6ee66df175de19bf7aaf4d92f4c6f8976b6
yogurt14r <smurfinho03@gmail.com>
2025-03-13 23:03:07 +0900

    update Button

--- project2/tsi-course/src/components/Button.jsx
+++ project2/tsi-course/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ onClick, children }) => {
     return (
-        <button onClick={onClick} className="bg-white hover:bg-[#00e0c7] text-[#000] px-4 py-2 rounded-full transition duration-300">
+        <button onClick={onClick} className="bg-white hover:bg-[#00e0c7] text-[#000] px-4 py-2 rounded-[20px] transition duration-300">
             {children}
         </button>
     );
