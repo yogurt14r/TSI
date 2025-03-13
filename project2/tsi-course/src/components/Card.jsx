f485d26f72199c6dd2e9c150a25be276b1e543a3
yogurt14r <smurfinho03@gmail.com>
2025-03-13 23:03:21 +0900

    Update Card.jsx

--- project2/tsi-course/src/components/Card.jsx
+++ project2/tsi-course/src/components/Card.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import samsa from '../assets/samsa.png'
 
 const Card = () => {
     return (
-        <div></div>
+        <div className="w-[220px] mr-[20px] border-1">
+            <div className="flex flex-col justify-center mb-[60px]">
+                <img src={samsa} alt="samsa" />
+                <div>asderrge</div>
+                <div>цена</div>
+                <div>в корзину</div>
+            </div>
+        </div>
     );
 };
 
